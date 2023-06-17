5375e792d674df18f5857b69b18552baa8145ee6
Arthur Marques <arthur.marques@sgaseguros.com.br>
2023-06-17 13:58:39 -0300

    Entrega

--- cadastro_usuario.php
+++ cadastro_usuario.php
@@ -5,76 +5,90 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Splash | Cadastro</title>
+    <link rel='stylesheet' type='text/css' href='css/style.css'>
+    <link rel="stylesheet" href="css/cadastro_usuario.css">
+
 </head>
 <body>
-    <?php
-    session_start();
-    require "topo.php";
-    ?>
+    <div class="container">
+        <div class="registration-form">
+            <h1>Cadastro</h1>
+            <form action="inserir_usuario.php" method="POST">
+                <input type="hidden" name="funcao" value="2">
+                <label for="nome">Nome completo</label>
+                <input type="text" name="nome" maxlength="80">
 
-    <form action="inserir_usuario.php" method="POST">
-        <input type="hidden" name="funcao" value="2"><br>
+                <label for="cpf">CPF</label>
+                <input type="text" name="cpf" maxlength="11">
 
-        <label for="nome">Nome</label><br>
-        <input type="text" name="nome" maxlength="80"><br>
+                <label for="cep">CEP</label>
+                <input type="text" name="cep" maxlength="8">
 
-        <label for="cpf">CPF</label><br>
-        <input type="text" name="cpf" maxlength="11"><br>
+                <label for="rua">Rua</label>
+                <input type="text" name="rua" maxlength="50">
 
-        <label for="cep">CEP</label><br>
-        <input type="text" name="cep" maxlength="8"><br>
-        
-        <label for="rua">Rua</label><br>
-        <input type="text" name="rua" maxlength="50"><br>
+                <div style="display: flex; gap: 10px;">
+                    <div style="flex-grow: 1;">
+                        <label for="numeroPredio">Número</label>
+                        <input type="number" name="numeroPredio" maxlength="11">
+                    </div>
+                    
+                    <div style="flex-grow: 1;">
+                        <label for="bairro">Bairro</label>
+                        <input type="text" name="bairro" maxlength="50">
+                    </div>
+                </div>
 
-        <label for="numeroPredio">Número do prédio</label><br>
-        <input type="number" name="numeroPredio" maxlength="11"><br>
+                <div style="display: flex; gap: 10px;">
+                    <div style="flex-grow: 1;">
+                        <label for="cidade">Cidade</label>
+                        <input type="text" name="cidade" maxlength="50">
+                    </div>
 
-        <label for="bairro">Bairro</label><br>
-        <input type="text" name="bairro" maxlength="50"><br>
+                    <div style="flex-grow: 1;">
+                        <label for="estado">Estado (UF)</label>
+                        <select type="text" name="estado" maxlength="2" required>
+                            <option value="">Selecione...</option>
+                            <option value="AC">AC</option>
+                            <option value="AL">AL</option>
+                            <option value="AP">AP</option>
+                            <option value="AM">AM</option>
+                            <option value="BA">BA</option>
+                            <option value="CE">CE</option>
+                            <option value="DF">DF</option>
+                            <option value="ES">ES</option>
+                            <option value="GO">GO</option>
+                            <option value="MA">MA</option>
+                            <option value="MT">MT</option>
+                            <option value="MS">MS</option>
+                            <option value="MG">MG</option>
+                            <option value="PA">PA</option>
+                            <option value="PB">PB</option>
+                            <option value="PR">PR</option>
+                            <option value="PE">PE</option>
+                            <option value="PI">PI</option>
+                            <option value="RJ">RJ</option>
+                            <option value="RN">RN</option>
+                            <option value="RS">RS</option>
+                            <option value="RO">RO</option>
+                            <option value="RR">RR</option>
+                            <option value="SC">SC</option>
+                            <option value="SP">SP</option>
+                            <option value="SE">SE</option>
+                            <option value="TO">TO</option>
+                        </select>
+                    </div>
+                </div>
 
-        <label for="cidade">Cidade</label><br>
-        <input type="text" name="cidade" maxlength="50"><br>
+                <label for="telefone">Telefone</label>
+                <input type="text" name="telefone" maxlength="20">
 
-        <label for="estado">Estado (UF)</label><br>
-        <select type="text" name="estado" maxlength="2" required>
-            <option value="">Selecione...</option>
-            <option value="AC">AC</option>
-            <option value="AL">AL</option>
-            <option value="AP">AP</option>
-            <option value="AM">AM</option>
-            <option value="BA">BA</option>
-            <option value="CE">CE</option>
-            <option value="DF">DF</option>
-            <option value="ES">ES</option>
-            <option value="GO">GO</option>
-            <option value="MA">MA</option>
-            <option value="MT">MT</option>
-            <option value="MS">MS</option>
-            <option value="MG">MG</option>
-            <option value="PA">PA</option>
-            <option value="PB">PB</option>
-            <option value="PR">PR</option>
-            <option value="PE">PE</option>
-            <option value="PI">PI</option>
-            <option value="RJ">RJ</option>
-            <option value="RN">RN</option>
-            <option value="RS">RS</option>
-            <option value="RO">RO</option>
-            <option value="RR">RR</option>
-            <option value="SC">SC</option>
-            <option value="SP">SP</option>
-            <option value="SE">SE</option>
-            <option value="TO">TO</option>
-        </select><br>
+                <label for="senha">Senha</label>
+                <input type="password" name="senha" maxlength="50">
 
-        <label for="telefone">Telefone</label><br>
-        <input type="text" name="telefone" maxlength="20"><br>
-
-        <label for="senha">Senha</label><br>
-        <input type="password" name="senha" maxlength="50"><br>
-
-        <input type="submit">
-    </form><br>
+                <input type="submit" value="Cadastrar">
+            </form>
+        </div>
+    </div>
 </body>
 </html>
--- login.php
+++ login.php
@@ -6,18 +6,102 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Splash | Login</title>
 </head>
-<body>
-    <?php
-    session_start();
-    require "topo.php";
-    ?>
-    <form action="validar_login.php" method="POST">
-        <label for="cpf">CPF</label><br>
-        <input type="text" name="cpf"><br>
-        <label for="senha">Senha</label><br>
-        <input type="password" name="senha"><br>
-        <input type="submit">
-    </form><br>
-    <a href="cadastro_usuario.php">Casdastre-se</a>
-</body>
-</html>
+<style>
+body {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 20px;
+    background-color: #f2f2f2;
+}
+
+.container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+}
+
+.login-form {
+    background-color: #ffffff;
+    padding: 20px;
+    border-radius: 5px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    width: 300px;
+}
+
+.login-form h1 {
+    text-align: center;
+    margin-bottom: 20px;
+}
+
+.login-form label {
+    display: block;
+    margin-bottom: 5px;
+}
+
+.login-form input[type="text"],
+.login-form input[type="password"] {
+    width: 100%;
+    padding: 10px;
+    margin-bottom: 15px;
+    border: 1px solid #ccc;
+    border-radius: 3px;
+    box-sizing: border-box;
+}
+
+.login-form input[type="submit"] {
+    background-color: #4caf50;
+    color: #ffffff;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 3px;
+    cursor: pointer;
+}
+
+.login-form a {
+    display: block;
+    text-align: center;
+    margin-top: 15px;
+    text-decoration: none;
+    color: #4caf50;
+}
+
+.text-container {
+    background-image: url("capa_loguin.jpg");
+    background-size: cover;
+    background-position: center;
+    padding: 20px;
+    border-radius: 5px;
+    color: #ffffff;
+    text-align: center;
+}
+
+.cruise-images {
+    display: flex;
+    justify-content: center;
+    margin-top: 20px;
+}
+
+.cruise-images img {
+    width: 100px;
+    height: 100px;
+    margin: 0 10px;
+    border-radius: 50%;
+}
+</style>
+    <body>
+    <div class="container">
+        <div class="login-form">
+            <h1>Login</h1>
+            <form action="validar_login.php" method="POST">
+                <label for="cpf">CPF</label>
+                <input type="text" name="cpf">
+                <label for="senha">Senha</label>
+                <input type="password" name="senha">
+                <input type="submit" value="Login">
+            </form>
+            <a href="cadastro_usuario.php">Cadastre-se</a>
+        </div>
+    </div
+
+
